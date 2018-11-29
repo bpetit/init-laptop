e599181a8d2487b0bdbf96e5ac2fd1b945dfa95f
Benoit Petit <bpetit@b0rk.in>
2018-11-30 00:06:54 +0100

    Adding slack and vim

--- init-laptop.sh
+++ init-laptop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get install vim python-pip -y
-pip install ansible
+apt install git ansible -y
+mkdir -p $HOME/git
+cd $HOME/git
+git clone https://github.com/bpetit/init-laptop.git
+cd init-laptop
+cp host_vars/localhost.yml.sample host_vars/localhost.yml
+sed -i "s/johndoe/${WHOAMI}/" host_vars/localhost.yml
 ansible-playbook init-laptop.yml
