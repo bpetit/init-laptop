4b836c64475f025e69f55246cff30b34b07ab117
Benoit Petit <bpetit@b0rk.in>
2018-12-08 16:47:52 +0100

    Improved vim configuration steps and init script

--- init-laptop.sh
+++ init-laptop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-apt install git ansible -y
+WHOAMI=$(whoami)
+
+sudo apt update
+sudo apt install git python-pip -y
+sudo pip install ansible
 mkdir -p $HOME/git
 cd $HOME/git
 git clone https://github.com/bpetit/init-laptop.git
 cd init-laptop
 cp host_vars/localhost.yml.sample host_vars/localhost.yml
 sed -i "s/johndoe/${WHOAMI}/" host_vars/localhost.yml
-ansible-playbook init-laptop.yml
+ansible-playbook --ask-become-pass init-laptop.yml
