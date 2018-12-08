5873bc87f898d0a62f8f72e93654e4f53cf13af9
Benoit Petit <bpetit@b0rk.in>
2018-12-08 17:25:54 +0100

    Improved script and added redshift and mfa configuration

--- init-laptop.sh
+++ init-laptop.sh
@@ -7,8 +7,13 @@ sudo apt install git python-pip -y
 sudo pip install ansible
 mkdir -p $HOME/git
 cd $HOME/git
-git clone https://github.com/bpetit/init-laptop.git
-cd init-laptop
+if [[ -d init-laptop ]]; then
+  cd init-laptop && git pull origin master
+else
+  git clone https://github.com/bpetit/init-laptop.git && cd init-laptop
+fi
 cp host_vars/localhost.yml.sample host_vars/localhost.yml
 sed -i "s/johndoe/${WHOAMI}/" host_vars/localhost.yml
+wget https://files.pythonhosted.org/packages/source/m/mitogen/mitogen-0.2.3.tar.gz
+tar xzvf mitogen-0.2.3.tar.gz
 ansible-playbook --ask-become-pass init-laptop.yml
